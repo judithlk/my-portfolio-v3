94a854f630572cc9590b6d35c58ad81df8d89bcc
Judith Yusuf <judithyusuf00@gmail.com>
2026-02-21 15:19:55 +0100

    new resume

--- lib/experience.ts
+++ lib/experience.ts
@@ -19,7 +19,7 @@ export const EXPERIENCE = [
     company: "NAS Satelite",
     role: "Software Developer",
     backlink: "https://ng.linkedin.com/in/nas-satellite-251828216",
-    duration: `Apr 2024 ${String.fromCharCode(8212)} Mar 2025`,
+    duration: `Apr 2024 ${String.fromCharCode(8212)} May 2025`,
     description:
       "Developed and maintained functional, optimized web applications for diverse business needs. Collaborated closely with backend teams to integrate RESTful APIs, debug complex issues, refactor codebases, and deliver accessible, visually appealing, and high-performance user interfaces.",
     technologies: [
